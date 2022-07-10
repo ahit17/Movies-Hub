35e72c82b28f24134041a87986afe0e69b18847e
ahit17 <ahitrik2001@gmail.com>
2022-07-10 23:11:50 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>Developed by Ahitagni Pai @2k22</div>
+    </div>
+  );
+};
+
+export default Footer;
